e82d1eb1e48342cef1fb720f3cabd5473772b3db
sethupathyraj27 <sethupathyraj27@gmail.com>
2023-01-06 17:17:59 +0530

    maven project

---
+++ src/main/java/pageObjects/User_POM.java
@@ -0,0 +1,5 @@
+package pageObjects;
+
+public class User_POM {
+
+}
---
+++ src/main/java/testCase/Test_User.java
@@ -0,0 +1,14 @@
+package testCase;
+
+import org.testng.annotations.Test;
+
+import commonFun.CommonFunctions;
+
+public class Test_User extends CommonFunctions {
+	@Test
+	public void check() throws InterruptedException {
+//		Thread.sleep(5000);
+		System.out.println("testUserClass");
+		System.out.println("checkUser");
+	}
+}
